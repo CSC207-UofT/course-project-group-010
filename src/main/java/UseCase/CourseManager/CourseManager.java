fbbb457b0dac1b13ea4780d7cfcf7a13e3e8702e
noahsub <48663769+noahsub@users.noreply.github.com>
2021-10-13 17:55:24 -0400

    Draft for: CourseManager.java CoursePage.java
    [2021-10-13 ; 17:54:09]

--- src/main/java/UseCase/CourseManager/CourseManager.java
+++ src/main/java/UseCase/CourseManager/CourseManager.java
@@ -1,4 +1,28 @@
 package UseCase.CourseManager;
 
+import Entity.Instructor;
+import UseCase.CoursePage.CoursePage;
+
 public class CourseManager {
+    private CoursePage coursePage;
+
+    public CourseManager (CoursePage coursePage)
+    {
+        this.coursePage = coursePage;
+    }
+
+    public fliterData(Instructor instructor, int year)
+    {
+        if (this.coursePage.getInstructors().contains(instructor) && this.coursePage.getYears().contains(year))
+        {
+            this.coursePage.setInstructor(instructor);
+            this.coursePage.setYear(year);
+        }
+    }
+
+    public leaveReview(Student student)
+    {
+        // if Student is authorized
+        // then compute new review
+    }
 }
