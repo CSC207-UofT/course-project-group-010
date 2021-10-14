ad4a48bcfb7b3c42710d63f3ec073ac64e0ef0b9
dungwoong <78326933+dungwoong@users.noreply.github.com>
2021-10-14 13:59:55 -0400

    Updated Database abstract class
    Database implementation was decided here

--- src/main/java/Interface/IDBSaveable.java
+++ src/main/java/Interface/IDBSaveable.java
@@ -5,5 +5,11 @@ public interface IDBSaveable {
      * save the information that the user entries into course database
      */
     String giveData();
+
+    /**
+     * gets some sort of id. IDs should be unique
+     * @return
+     */
+    String getID();
 }
 
