582c07156d8ac3741832abc97d3c649607a66212
Nima <63063518+csclover420@users.noreply.github.com>
2021-10-15 21:04:19 -0400

    Added Auth Helper
    Not sure what coursemanager looks like yet so I just used a.dictionary to access the dictionary that I would need in AuthHelper.

---
+++ src/main/java/UseCase/AuthHelper.java
@@ -0,0 +1,18 @@
+package UseCase;
+
+import Controller.Commands.CommandExceptions.CommandNotAuthorizedException;
+import Entity.User;
+
+public class AuthHelper {
+    public boolean checkAuth(Authorizable a, int userPermission, String action) throws CommandNotAuthorizedException {
+
+        if ( !(a.dictionary.contains(action)) ){
+            return false;
+        }
+
+        if (!a.dictionary.contains(action) & !(userPermission >= a.dictionary.get(action))) {
+            throw CommandNotAuthorizedException;}
+        return true;
+    }
+
+}
