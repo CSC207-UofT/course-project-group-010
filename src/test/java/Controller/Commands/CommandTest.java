8d9f001572284b0bd7d3f03cc3528ef557368b45
dungwoong <78326933+dungwoong@users.noreply.github.com>
2021-10-17 19:37:15 -0400

    added tests
    uncommented command related tests, they should work.

--- src/test/java/Controller/Commands/CommandTest.java
+++ src/test/java/Controller/Commands/CommandTest.java
@@ -1,30 +1,30 @@
-//package Controller.Commands;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-///**
-// * Basic test of the command classes and the commandExecutor
-// */
-//public class CommandTest {
-//    CommandExecutor ce;
-//
-//    @Before
-//    public void setup() {
-//        ce = CommandExecutor.getInstance();
-//    }
-//
-//    @Test(timeout = 100)
-//    public void testNoCommand() {
-//        CommandRequest request = new CommandRequest("asdf");
-//        assertEquals(ce.processRequest(request), "Command not Found");
-//    }
-//
-//    @Test(timeout = 100)
-//    public void testInvalidArgs() {
-//        CommandRequest request = new CommandRequest("print asdf agaj fklaj");
-//        assertEquals(ce.processRequest(request), "Invalid number of Arguments");
-//    }
-//}
+package Controller.Commands;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Basic test of the command classes and the commandExecutor
+ */
+public class CommandTest {
+    CommandExecutor ce;
+
+    @Before
+    public void setup() {
+        ce = CommandExecutor.getInstance();
+    }
+
+    @Test(timeout = 100)
+    public void testNoCommand() {
+        CommandRequest request = new CommandRequest("asdf");
+        assertEquals(ce.processRequest(request), "Command not Found");
+    }
+
+    @Test(timeout = 100)
+    public void testInvalidArgs() {
+        CommandRequest request = new CommandRequest("print asdf agaj fklaj");
+        assertEquals(ce.processRequest(request), "Invalid number of Arguments");
+    }
+}
