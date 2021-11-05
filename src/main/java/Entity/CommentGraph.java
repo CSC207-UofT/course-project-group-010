7944ae26b835f611170126f4fdec47b2f8081c29
noahsub <48663769+noahsub@users.noreply.github.com>
2021-11-05 09:42:42 -0400

    Added instance attributes to CommentGraph.java and subclasses.

--- src/main/java/Entity/CommentGraph.java
+++ src/main/java/Entity/CommentGraph.java
@@ -2,14 +2,25 @@ package Entity;
 
 import java.util.HashMap;
 
-public class CommentGraph {
+public class CommentGraph
+{
+    private HashMap<String, Comment> vertices;
+    private int size;
+    private Comment head;
 
-    private class Comment{
-
+    private class Comment
+    {
+        private String id;
+        private String text;
+        private int depth;
+        private int upvote;
+        private Boolean isQuestion;
+        private Comment next;
+        private Comment prev;
     }
 
     private class DijkstraAttributes
     {
-
+        private Boolean visited;
     }
 }
