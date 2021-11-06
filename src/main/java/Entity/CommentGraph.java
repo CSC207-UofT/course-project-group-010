9c9d4479a9b41ce69473b3a6aa86916288b6b66e
noahsub_laptop <noahsub@outlook.com>
2021-11-06 19:49:34 -0400

    Moved id generation method to helper class.

--- src/main/java/Entity/CommentGraph.java
+++ src/main/java/Entity/CommentGraph.java
@@ -115,26 +115,12 @@ public class CommentGraph
     private String genUniqueId() {
         // makes sure that the id is in fact unique, there is a very small chance that this code will actually run
         // due to the number of possibilities, but this is here just in case.
-        String uniqueId = genId();
+        CommentGraphHelper genIdHelper = new CommentGraphHelper();
+        String uniqueId = genIdHelper.genId();
         while (vertices.containsKey(uniqueId)) {
-            uniqueId = genId();
+            uniqueId = genIdHelper.genId();
         }
         return uniqueId;
-    }
-
-    private String genId() {
-        char[] alphabet = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
-                'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-                'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
-        Random rand = new Random();
-        char[] encodedChars = new char[5];
-        for (int i = 0; i < 5; i++) {
-            encodedChars[i] = alphabet[rand.nextInt(62)];
-        }
-
-        return new String(encodedChars);
     }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
