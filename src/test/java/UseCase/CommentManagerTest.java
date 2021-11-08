5dada4ba9ce6dce287441181997d387ccd293097
noahsub_laptop <noahsub@outlook.com>
2021-11-08 11:51:54 -0500

    Added comments to tests.

--- src/test/java/UseCase/CommentManagerTest.java
+++ src/test/java/UseCase/CommentManagerTest.java
@@ -1,25 +1,29 @@
 package UseCase;
 
-
 import Entity.CommentGraph;
 import UseCase.CommentManager.CommentManager;
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-
-import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class CommentManagerTest
 {
+    /**
+     * This method is used for building a sample CommentGraph. THIS IS NOT HOW A COMMENTGRAPH SHOULD NORMALLY BE
+     * CONSTRUCTED. This implementation calls private methods which should not be used outside their respective
+     * classes. In this case I had no choice due to the random nature of the id generation which would make it
+     * impossible to test otherwise.
+     *
+     * @return A sample CommentGraph that is used for tests.
+     * @throws NoSuchMethodException
+     * @throws ClassNotFoundException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
     public static CommentGraph sampleCommentGraphBuilder() throws NoSuchMethodException, ClassNotFoundException, InvocationTargetException, IllegalAccessException
     {
+        // Get private methods from commentGraph Class
         Class<?> commentGraphClass = Class.forName("Entity.CommentGraph");
 
         Method createCommentMethod = commentGraphClass.getDeclaredMethod("createComment", String.class, String.class, String.class);
@@ -31,8 +35,10 @@ public class CommentManagerTest
         Method linkMethod = commentGraphClass.getDeclaredMethod("link", String.class, CommentGraph.Comment.class);
         linkMethod.setAccessible(true);
 
+        // create an empty Comment Graph
         CommentGraph CSC207Thread = new CommentGraph("Questions", "Instructor");
 
+        // Manually create comments and links, this should not be done, only done for the sake of testing.
         CommentGraph.Comment question1 = (CommentGraph.Comment) createCommentMethod.invoke(CSC207Thread, "id1", "How did you like the course?", "Instructor");
         addVertexMethod.invoke(CSC207Thread, "id1", question1);
         linkMethod.invoke(CSC207Thread, "root", question1);
@@ -95,6 +101,15 @@ public class CommentManagerTest
         return CSC207Thread;
     }
 
+    /**
+     * Tests that the displayEntireThread method displays the String representation of the entire CommentGraph up to its
+     * full depth.
+     *
+     * @throws ClassNotFoundException
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
     @Test
     public void testCompleteStringRepresentationFullDepth() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException
     {
@@ -127,9 +142,17 @@ public class CommentManagerTest
         assert (expected.equals(actual));
     }
 
+    /**
+     * Tests that the displayEntireThread method displays the String representation of the entire CommentGraph up to
+     * depth 2.
+     *
+     * @throws ClassNotFoundException
+     * @throws InvocationTargetException
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     */
     @Test
-    public void testCompleteStringRepresentationDepth2() throws ClassNotFoundException,
-            InvocationTargetException, NoSuchMethodException, IllegalAccessException
+    public void testCompleteStringRepresentationDepth2() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException
     {
         CommentManager commentManager = new CommentManager(sampleCommentGraphBuilder());
         String expected = commentManager.displayEntireThread(true, 2);
@@ -156,13 +179,21 @@ public class CommentManagerTest
                 No, the course was perfect.
                 ↑ 1 ↓
         */
-        
+
         assert (expected.equals(actual));
     }
 
+    /**
+     * Tests that the displaySubsetThread method displays the String representation of a subset of a CommentGraph
+     * up to the full depth.
+     *
+     * @throws ClassNotFoundException
+     * @throws InvocationTargetException
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     */
     @Test
-    public void testSubsetStringRepresentationFullDepth() throws ClassNotFoundException,
-            InvocationTargetException, NoSuchMethodException, IllegalAccessException
+    public void testSubsetStringRepresentationFullDepth() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException
     {
         CommentManager commentManager = new CommentManager(sampleCommentGraphBuilder());
         String expected = commentManager.displaySubsetThread("id2", true, -1);
@@ -178,12 +209,21 @@ public class CommentManagerTest
             ↑ -4 ↓
         */
 
-        assert(expected.equals(actual));
+        assert (expected.equals(actual));
     }
 
+    /**
+     * Tests that the displaySubsetThread method displays the String representation of a subset of a CommentGraph up
+     * to depth 0.
+     *
+     * @throws NoSuchMethodException
+     * @throws ClassNotFoundException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
     @Test
-    public void testSubsetStringRepresentationDepth0() throws NoSuchMethodException, 
-    ClassNotFoundException, InvocationTargetException, IllegalAccessException{
+    public void testSubsetStringRepresentationDepth0() throws NoSuchMethodException, ClassNotFoundException, InvocationTargetException, IllegalAccessException
+    {
         CommentManager commentManager = new CommentManager(sampleCommentGraphBuilder());
         String expected = commentManager.displaySubsetThread("id2", true, 0);
         String actual = "↳ Instructor [id2]\nWhat did you find difficult in the course?\n↑ 4 ↓\n\n";
@@ -194,12 +234,20 @@ public class CommentManagerTest
         ↑ 4 ↓
         */
 
-        assert(expected.equals(actual));
+        assert (expected.equals(actual));
     }
 
+    /**
+     * Tests that path generation from one Comment to another Comment given that there is a valid path works properly.
+     *
+     * @throws NoSuchMethodException
+     * @throws ClassNotFoundException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
     @Test
-    public void testPath() throws NoSuchMethodException, 
-    ClassNotFoundException, InvocationTargetException, IllegalAccessException{
+    public void testPath() throws NoSuchMethodException, ClassNotFoundException, InvocationTargetException, IllegalAccessException
+    {
         CommentManager commentManager = new CommentManager(sampleCommentGraphBuilder());
         String expected = commentManager.getPath("root", "id6");
         String actual = "↳ Instructor [root]\nQuestions\n↑ 0 ↓\n\n    ↳ Instructor [id1]\n    How did you like the course?\n    ↑ 1 ↓\n\n        ↳ Student 1 [id4]\n        I really liked the course!\n        ↑ 5 ↓\n\n            ↳ Student 3 [id6]\n            Me too!\n            ↑ 7 ↓\n\n";
@@ -222,22 +270,39 @@ public class CommentManagerTest
                     ↑ 7 ↓
         */
 
-        assert(expected.equals(actual));
+        assert (expected.equals(actual));
     }
 
+    /**
+     * Tests that reply functionality works correctly.
+     *
+     * @throws NoSuchMethodException
+     * @throws ClassNotFoundException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
     @Test
-    public void testReply() throws NoSuchMethodException, 
-    ClassNotFoundException, InvocationTargetException, IllegalAccessException{
+    public void testReply() throws NoSuchMethodException, ClassNotFoundException, InvocationTargetException, IllegalAccessException
+    {
         CommentManager commentManager = new CommentManager(sampleCommentGraphBuilder());
         commentManager.replyToComment("id6", "Sample Reply", "Student 8");
-        assert(commentManager.getChildrenComments("id6").size() == 1);
+        assert (commentManager.getChildrenComments("id6").size() == 1);
     }
 
+    /**
+     * Tests that vote functionality works correctly.
+     *
+     * @throws NoSuchMethodException
+     * @throws ClassNotFoundException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
     @Test
-    public void testVote() throws NoSuchMethodException, ClassNotFoundException, InvocationTargetException, IllegalAccessException{
+    public void testVote() throws NoSuchMethodException, ClassNotFoundException, InvocationTargetException, IllegalAccessException
+    {
         CommentManager commentManager = new CommentManager(sampleCommentGraphBuilder());
         int initialVote = commentManager.getVote("id1");
         commentManager.vote("id1", true);
-        assert(commentManager.getVote("id1") == initialVote + 1);
+        assert (commentManager.getVote("id1") == initialVote + 1);
     }
 }
