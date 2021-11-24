8ad1360db8b48e3c53816efc87f50cebacf24060
fionali90 <jying.li@mail.utoronto.ca>
2021-11-23 22:58:30 -0500

    Add regex and program detail command line
    Add regex and program detail command line

--- src/main/java/Controller/Commands/NewUserCommand.java
+++ src/main/java/Controller/Commands/NewUserCommand.java
@@ -5,6 +5,7 @@ import exceptions.ArgumentException;
 import Controller.databasegetter.UserDatabaseGetter;
 import UseCase.UserManager;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -32,35 +33,49 @@ public class NewUserCommand extends Command {
         super.checkArgumentsNum(arguments);
         // super.checkUserExists(ce);
         Scanner in = new Scanner(System.in);
-        System.out.println("Type of user[student/instructor]:");
-        String argUserType = in.nextLine().toLowerCase();
-        if (!argUserType.equals("student") && !argUserType.equals("instructor")) {
-            throw new ArgumentException("Type must be [student/instructor], you entered " + argUserType);
+        System.out.println("Type of user[STUDENT/INSTRUCTOR] and Please use Capital letters:");
+        String argUserType = in.nextLine().toUpperCase();
+        if (!argUserType.equals("STUDENT") && !argUserType.equals("INSTRUCTOR")) {
+            throw new ArgumentException("Type must be [STUDENT/INSTRUCTOR], you entered " + argUserType);
         }
         System.out.println("Display name: ");
         String argDisplayName = in.nextLine();
         System.out.println("ID(no spaces)");
         String argId = in.nextLine().replace(" ", "");
+        boolean isSame = argId.matches("^[a-z][a-z0-9]*");
+        if (!isSame) {
+            throw new ArgumentException("Your entered ID is invaild, you entered " + argId);
+        }
         System.out.println("any spaces were removed. ID is " + argId);
-
         UserType desiredUserType = getUserType(argUserType);
+        System.out.println("Program Detail: " + "\n" + "Note : Choose from one of following options: ACCOUNTING, ACTUARIAL SCIENCE, ANTHROPOLOGY, APPLIED MATHEMATICS, APPLIED STATISTICS,COMPUTER SCIENCE, DATA SCIENCE");
+        String argProgramDetail = in.nextLine().toUpperCase();
+        if (!argProgramDetail.equals(" ")) {
+            HashMap<String, String> adddetail = new HashMap<>();
+            adddetail.put("programDetail", argProgramDetail);
+            UserManager um = new UserManager(desiredUserType, argDisplayName, argId, adddetail);
+            UserDatabaseGetter.getInstance().addEntry(um);
+            return "Added new user with ID " + um.getID() + " and name " + um.getUser().getdisplayName() + "\n" +
+                    "Run saveall to save this progress.";
+        } else {
+            UserManager um = new UserManager(desiredUserType, argDisplayName, argId);
+            UserDatabaseGetter.getInstance().addEntry(um);
+            return "Added new user with ID " + um.getID() + " and name " + um.getUser().getdisplayName() + "\n" +
+                    "Run saveall to save this progress.";
+        }
 
-        UserManager um = new UserManager(desiredUserType, argDisplayName, argId);
         // AuthHelper ah = new AuthHelper();
         // ah.checkAuth(um, ce.getUserManager(), "newuser");
         // No auth checks for now, because we have 0 users in the db right now which is unfortunate
-        UserDatabaseGetter.getInstance().addEntry(um);
-        return "Added new user with ID " + um.getID() + " and name " + um.getUser().getdisplayName() + "\n" +
-                "Run saveall to save this progress.";
     }
 
     private UserType getUserType(String argUserType) throws ArgumentException {
         UserType desiredUserType;
         switch (argUserType) {
-            case "student":
+            case "STUDENT":
                 desiredUserType = UserType.STUDENT;
                 break;
-            case "instructor":
+            case "INSTRUCTOR":
                 desiredUserType = UserType.INSTRUCTOR;
                 break;
             default:
