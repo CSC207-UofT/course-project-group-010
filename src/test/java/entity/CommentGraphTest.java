6de14a82eac316b36c241c381c21ac47fc125f57
noahsub <48663769+noahsub@users.noreply.github.com>
2021-12-04 12:29:34 -0500

    Added test for stringRepresentation

--- src/test/java/entity/CommentGraphTest.java
+++ src/test/java/entity/CommentGraphTest.java
@@ -65,5 +65,33 @@ public class CommentGraphTest
         assert actual == expected;
     }
 
+    public static String unEscapeString(String s){
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<s.length(); i++)
+            switch (s.charAt(i)){
+                case '\n': sb.append("\\n"); break;
+                case '\t': sb.append("\\t"); break;
+                // ... rest of escape characters
+                default: sb.append(s.charAt(i));
+            }
+        return sb.toString();
+    }
+
+    @Test
+    public void testStringRepresentation()
+    {
+        HashMap<String, List<String>> initialComments = new HashMap<>();
+        initialComments.put("sampleUser1", List.of("sampleText1"));
+        initialComments.put("sampleUser2", List.of("sampleText2"));
+        CommentGraph cg = new CommentGraph("Test", "Test", initialComments);
+
+        String expected = "> Test [id: root]\nTest\n[+] 0 [-]\n\n    > sampleUser1 [id: 7DU2C]\n    sampleText1\n  " +
+                "  [+] 0 [-]\n\n    > sampleUser2 [id: 0ZSJX]\n    sampleText2\n    [+] 0 [-]\n\n";
+        String actual = cg.stringRepresentation(cg.getVertices().get("root"), 0, 1, true);
+
+        assert expected.length() == actual.length();
+    }
+
+
 
 }
