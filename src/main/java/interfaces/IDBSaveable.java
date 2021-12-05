ebe2e84611e1581689289b61d61d349d6073b9c3
UVUO <UVUO>
2021-12-05 17:08:46 -0500

    Trivial update in progress report and IDBSaveable

--- src/main/java/interfaces/IDBSaveable.java
+++ src/main/java/interfaces/IDBSaveable.java
@@ -5,10 +5,5 @@ package interfaces;
  */
 public interface IDBSaveable {
 
-    /**
-     * gets some sort of id to save to the database. IDs should be unique
-     * @return the ID of user
-     */
-    String getID();
 }
 
